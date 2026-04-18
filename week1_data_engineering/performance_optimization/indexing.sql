da479afbd11a035ec2891d4df50fa0ad027629c4
Bhumika-tyagi005 <tbhumika664@gmail.com>
2026-04-18 11:57:46 -0400

    indexing.sql

---
+++ week1_data_engineering/performance_optimization/indexing.sql
@@ -0,0 +1,23 @@
+USE your_database_name;
+
+-- Indexes for fact table (most important)
+
+CREATE INDEX idx_fact_customer 
+ON fact_sales(customer_id);
+
+CREATE INDEX idx_fact_product 
+ON fact_sales(product_id);
+
+CREATE INDEX idx_fact_date 
+ON fact_sales(order_date);
+
+CREATE INDEX idx_fact_invoice 
+ON fact_sales(invoice_no);
+
+-- Indexes for dimension tables
+
+CREATE INDEX idx_customer_id 
+ON dim_customer(customer_id);
+
+CREATE INDEX idx_product_id 
+ON dim_product(product_id);
